d624445dda829c50ec5b37dac8f4b5cea87ee0ee
CarlosAmezcua <Amezcua041196@gmail.com>
2022-10-27 01:58:12 -0600

    update vista de propiedades
    Se agregó un listado de las propiedades que se tienen reigstradas en la BD y los botones de eliminar(Rojo) y actualizar(amarillo)

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,18 @@
 <?php
-    $resultado = $_GET['resultado'] ?? null;
 
+    // Importar la conexión de la BD
+    require '../includes/config/database.php';
+    $db = ConnectDB();
+
+    // Escribir query
+    $query = "SELECT * FROM propiedades";
+
+    // Consultar la BD
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    // MUESTRA MENSAJE CONDICIONAL
+    $resultado = $_GET['resultado'] ?? null;
+    // INCLUYE UN TEMPLATE
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -12,6 +24,37 @@
     <?php endif; ?>
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Image</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody> <!-- Mostrar los resultados de la BD-->
+        <?php while( $propiedad = mysqli_fetch_assoc($resultadoConsulta) ): ?>
+            <tr>
+                <td> <?php echo $propiedad['id']; ?> </td>
+                <td> <?php echo $propiedad['titulo']; ?> </td>
+                <td> <img src="/images/<?php echo $propiedad['imagen'];?>" class="imagen-tabla"> </td>
+                <td>$ <?php echo $propiedad['precio']; ?> </td>
+                <td>
+                    <a class="boton-rojo-block" href="#">Eliminar</a>
+                    <a class="boton-amarillo-block" href="#">Actualizar</a>
+                </td>
+            </tr>
+        <?php endwhile; ?>
+        </tbody>
+    </table>
 </main>
 
-<?php incluirTemplate('footer'); ?>
+<?php
+    // Cerrar la conexión de la BD
+    mysqli_close($db); // Opcional, sql detecta cuando se suspende el uso y la cierra automaticamente.
+
+    incluirTemplate('footer');
+
+?>
